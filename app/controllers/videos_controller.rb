0bc97c9303a2f925ecc9bd978717bec29d9e4db6
daisuke8000 <daisuke12108000@gmail.com>
2020-05-06 13:31:04 +0900

    room,videoモデルとマイグレーションファイルの作成、及びroomのviewまでの導線を実装

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,10 @@
 class VideosController < ApplicationController
 
+  def new
+    @video = Video.new
+  end
+
   def index
+    @videos = Video.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   root to: 'homes#index'
-  resources :videos
+  resources :rooms do
+    resources :videos
+  end
 end
