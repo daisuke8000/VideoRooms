04eacbbb3eee7ecbf68fb70e98bac5a6ccf8f058
daisuke8000 <daisuke12108000@gmail.com>
2020-05-07 03:19:25 +0900

    users_controllerの追加。users,videosのindexファイルの追加・アクセス確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  def index
-    @users = User.all
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   root to: 'homes#index'
-  resources :rooms do
-    resources :videos
-  end
+  resources :users
+  resources :rooms
+  resources :videos
 end
