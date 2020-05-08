0c28c7b99e8853ef5609cd4974e274021418273e
daisuke8000 <daisuke12108000@gmail.com>
2020-05-08 18:17:36 +0900

    user,roomのmodelのアソシエーションを実装

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index]
   def new
     @video = Video.new
   end
@@ -7,4 +7,25 @@ class VideosController < ApplicationController
   def index
     @videos = Video.all
   end
+
+  def create
+    @video = Video.new(video_params)
+    @video.room_id = @room.id # これで値渡せず
+    if @video.save
+      redirect_to videos_path, notice: "新規の動画が投稿されました。"
+    else
+      flash.now[:alert] = "動画の投稿に失敗しました。"
+      render :new
+    end
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:name)
+  end
+
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,7 @@
 class Video < ApplicationRecord
+  #====以下、関連付けの記述===================================================
+  # belongs_to => このmodelに紐づく "親のmodel" （Railsの命名規則から単数形で指定する）
+  # 詳細はER図参照（daisuke_portfolio1.puml）
+  #=======================================================================
+  belongs_to :room
 end
