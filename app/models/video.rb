5e9a59b7ce274c4697cd51136c9f9c38f60c842b
daisuke8000 <daisuke12108000@gmail.com>
2020-05-23 02:25:26 +0900

    正規表現の定数化

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,9 +1,10 @@
 class Video < ApplicationRecord
   belongs_to :room
 
-  def channel
-    if self.youtube_url != ""
-      return youtube_url.match(/v=(.{11})/)[1]
+  def fetch_from_url
+    if self.youtube_url.present?
+      url_id = 'v=(.{11})'
+      youtube_url.match(/#{url_id}/)[1]
     end
   end
 end
