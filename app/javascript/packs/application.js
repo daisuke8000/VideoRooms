7564da63e60853d9bf4573796e57f26daf3ac750
daisuke8000 <daisuke12108000@gmail.com>
2020-05-27 00:32:51 +0900

    AdminLTE3.0.0-alpha.2の適用を確認

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap/dist/js/bootstrap")
+require("jquery/dist/jquery.min")
+require("bootstrap/dist/js/bootstrap.min")
+require("admin-lte/dist/js/adminlte.min")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
